c18422cd75a44bb18cdc7a527c03929b418fe74b
Stephanie <steph118@protonmail.com>
2023-05-10 16:39:52 +0800

    revised booking page

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { Client } = require('pg');
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
+const passport = require("passport");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const bcrypt = require("bcrypt");
+const path = require("path");
+
 const saltRounds = 10;
 
 (async () => {
@@ -11,16 +15,27 @@ const saltRounds = 10;
     })
     await client.connect()
 
-    app.use(express.static("public"));
+
     app.use(bodyParser.urlencoded({ extended: false }));
+
+    //Use static file
+    app.use(express.static(path.join(__dirname, '../')));
 
     /**********************Routes*****************************/
 
-    app.get('/healthcheck', (req, res) => {
-        res.send('OK')
-    });
+    //     app.get('/healthcheck', (req, res) => {
+    //         res.send('OK')
+    //     });
 
+    //     app.get('/auth/google',
+    //     passport.authenticate('google', { scope: ['profile'] }));
 
+    //    app.get('/auth/google/index', 
+    //     passport.authenticate('google', { failureRedirect: '/login' }),
+    //     function(req, res) {
+    //       // Successful authentication, redirect home.
+    //       res.redirect('/');
+    //     });
 
 
 
@@ -31,43 +46,144 @@ const saltRounds = 10;
     /*******************Log In & Register***********************/
 
     app.post("/login", async (req, res) => {
-        const { email, password } = req.body;
 
-        const foundUser = await client.query(`SELECT * FROM users WHERE email = $1 AND password = $2`, [email, password]);
+        try {
+            const { email, password } = req.body;
 
-        if (foundUser.rows.length) {
-            const foundUsername = foundUser.rows[0].username;
-            res.send('Log in successful! Welcome back, ' + foundUsername + '.');
-        } else {
-            res.send('unable to find record');
+            const foundUser = await client.query(`SELECT * FROM users WHERE email = $1`, [email]);
+
+            if (foundUser.rows.length) {
+                const foundUserPw = foundUser.rows[0].password;
+                const match = await bcrypt.compare(req.body.password, foundUserPw);
+
+                if (match) {
+                    res.redirect("http://127.0.0.1:3000");
+                } else {
+                    throw new Error("password doesn't match")
+                }
+            } else {
+                throw new Error("user not found")
+            }
+
+        } catch (err) {
+            res.send(err.message);
         }
+
+    });
+
+    app.post("/register", async (req, res) => {
+        const username = req.body.username;
+        const email = req.body.email;
+
+        try {
+            const hash = await bcrypt.hash(req.body.password, saltRounds);
+
+            const newUser = 'INSERT INTO users(username, email, password) VALUES($1, $2, $3) RETURNING *';
+            const input = [username, email, hash];
+
+            const register = await client.query(newUser, input);
+            // const newUsername = register.rows[0].username;
+
+            if (register.rows.length) {
+                res.send(register.rows[0].id);
+            }
+            // res.redirect("http://127.0.0.1:3000");
+
+        } catch (err) {
+            console.log(err);
+        }
+
     });
 
 
-    app.post("/register", async (req, res) => {
+    /********************Pets Ride Form***************************/
 
-        const username = req.body.username;
-        const email = req.body.email;
-        const password = req.body.password;
-
-        const newUser = 'INSERT INTO users(username, email, password) VALUES($1, $2, $3) RETURNING *';
-        const input = [username, email, password];
-
+    app.post("/ride-booking", async (req, res) => {
         try {
-            const register = await client.query(newUser, input);
-            const newUsername = register.rows[0].username;
+            const date4 = req.body.date4;
+            const time4 = req.body.time4;
+            const pickUp4 = req.body.pickup4;
+            const dropOff4 = req.body.dropoff4;
+            const numberOfPets4 = req.body.number4;
 
-            res.send('Register successful! Hi, ' + newUsername + '.');
+            const newRide = 'INSERT INTO ride(date, time, pickup, dropoff, numberofpets) VALUES($1, $2, $3, $4, $5) RETURNING *';
+            const inputRide = [date4, time4, pickUp4, dropOff4, numberOfPets4]
+            const bookRide = await client.query(newRide, inputRide);
+            console.log(bookRide.rows[0]);
+            res.redirect('http://127.0.0.1:3000/html/success.html')
 
         } catch (err) {
             console.log(err);
         }
     });
 
+    /******************Pets Grooming Form**********************/
 
+    app.post("/grooming-booking", async (req, res) => {
+        try {
+            const date3 = req.body.date3;
+            const time3 = req.body.time3;
+            const numberOfPets3 = req.body.number3;
 
+            const newGrooming = 'INSERT INTO grooming(date, time, numberofpets) VALUES($1, $2, $3) RETURNING *';
+            const inputGrooming = [date3, time3, numberOfPets3]
+            const bookGrooming = await client.query(newGrooming, inputGrooming);
+            console.log(bookGrooming.rows[0]);
+            res.redirect('http://127.0.0.1:3000/html/success.html')
 
-app.listen(3001, (req, res) => {
-    console.log("server started at port 3001");
-});
-}) ()
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+    /**********************Pet Sitting***************************/
+
+    app.post("/sitting-booking", async (req, res) => {
+        try {
+            const date1 = req.body.date1;
+            const time1 = req.body.time1;
+            const frequency1 = req.body.frequency1;
+            const location1 = req.body.location1;
+            const numberOfPets1 = req.body.number1;
+            const district1 = req.body.district1;
+
+            const newSitting = 'INSERT INTO sitting (date, time, frequency, location, numberofpets, district) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+            const inputSitting = [date1, time1, frequency1, location1, numberOfPets1, district1]
+            const bookSitting = await client.query(newSitting, inputSitting);
+            console.log(bookSitting.rows[0]);
+            res.redirect('http://127.0.0.1:3000/html/success.html')
+
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+    // (date, time, frequency, location, numberofpets, district)
+
+    /**********************Dog Walking***************************/
+
+    app.post("/walking-booking", async (req, res) => {
+        try {
+            const date2 = req.body.date2;
+            const time2 = req.body.time2;
+            const frequency2 = req.body.frequency2;
+            const duration2 = req.body.duration2;
+            const numberOfPets2 = req.body.number2;
+           
+
+            const newWalking = 'INSERT INTO walking(date, time, frequency, duration, numberofpets) VALUES($1, $2, $3, $4, $5) RETURNING *';
+            const inputWalking = [date2, time2, frequency2, duration2, numberOfPets2]
+            const bookWalking = await client.query(newWalking, inputWalking);
+            console.log(bookWalking.rows[0]);
+            res.redirect('http://127.0.0.1:3000/html/success.html')
+
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+    /**********************Server start at port 3001****************/
+    app.listen(3001, (req, res) => {
+        console.log("server started at port 3001");
+    });
+})()
---
+++ js/booking.js
@@ -0,0 +1,156 @@
+const chosenService = document.querySelectorAll("#service-choice .choice-card");
+
+
+chosenService.forEach(chosenService => {
+    chosenService.addEventListener('click', () => {
+        var prevChoice = document.querySelector(".action-card");
+
+        const petSitting = document.querySelectorAll(".choice-card")[0];
+        const dogWalking = document.querySelectorAll(".choice-card")[1];
+        const petsGrooming = document.querySelectorAll(".choice-card")[2];
+        const petsRide = document.querySelectorAll(".choice-card")[3];
+
+        const planHeading = document.querySelector("#plan-heading");
+        const planChoice = document.querySelector("#plan-choice");
+        const sittingPlan = document.querySelectorAll("#plan-choice")[0];
+        const walkingPlan = document.querySelectorAll("#plan-choice")[1];
+        const groomingPlan = document.querySelectorAll("#plan-choice")[2];
+
+
+        const sittingForm = document.querySelectorAll("#booking-form")[0];
+        const walkingForm = document.querySelectorAll("#booking-form")[1];
+        const groomingForm = document.querySelectorAll("#booking-form")[2];
+        const rideForm = document.querySelectorAll("#booking-form")[3];
+
+        const chosenPlan = document.querySelectorAll("#plan-choice .choice-card");
+
+        if (prevChoice) {
+            prevChoice.classList.remove("action-card");
+
+        } chosenService.classList.add("action-card");
+
+
+        const OTP = document.querySelectorAll(".choice-card")[4];
+        const RDP = document.querySelectorAll(".choice-card")[5];
+        const ROP = document.querySelectorAll(".choice-card")[6];
+        const EODP = document.querySelectorAll(".choice-card")[7];
+        const DP = document.querySelectorAll(".choice-card")[8];
+        const RAP = document.querySelectorAll(".choice-card")[9];
+        const shower = document.querySelectorAll(".choice-card")[10];
+        const hair = document.querySelectorAll(".choice-card")[11];
+        const spa = document.querySelectorAll(".choice-card")[12];
+
+
+        if (petSitting === chosenService) {
+            planHeading.style.visibility = 'visible';
+            sittingPlan.style.display = 'flex';
+            walkingPlan.style.display = 'none';
+            groomingPlan.style.display = 'none';
+
+            chosenPlan.forEach(chosenPlan => {
+                chosenPlan.addEventListener('click', () => {
+                    var prevChoice = document.querySelector(".action-card");
+                    if (prevChoice) {
+                        prevChoice.classList.remove("action-card");
+                    } chosenPlan.classList.add("action-card");
+                    var frequencyLabel1 = document.querySelectorAll(".frequency-label")[0];
+                    var frequency1 = document.querySelectorAll(".frequency")[0];
+                    if (OTP === chosenPlan) {
+                        sittingForm.style.display = 'flex';
+                        walkingForm.style.display = 'none';
+                        groomingForm.style.display = 'none';
+                        rideForm.style.display = 'none';
+                        frequencyLabel1.style.display = 'none';
+                        frequency1.style.display = 'none';
+                    } else {
+                        sittingForm.style.display = 'flex'; 
+                        walkingForm.style.display = 'none';
+                        groomingForm.style.display = 'none';
+                        rideForm.style.display = 'none';
+                        frequencyLabel1.style.display = '';
+                        frequency1.style.display = '';
+                    }
+                })
+            })
+        }
+
+        if (dogWalking === chosenService) {
+            planHeading.style.visibility = 'visible';
+            walkingPlan.style.display = 'flex';
+            sittingPlan.style.display = 'none';
+            groomingPlan.style.display = 'none';
+
+            const m15 = document.querySelector(".m15");
+            const m30 = document.querySelector(".m30");
+            const m45 = document.querySelector(".m45");
+            const hr = document.querySelector(".hr");
+
+            chosenPlan.forEach(chosenPlan => {
+                chosenPlan.addEventListener('click', () => {
+                    var prevChoice = document.querySelector(".action-card");
+                    if (prevChoice) {
+                        prevChoice.classList.remove("action-card");
+                    } chosenPlan.classList.add("action-card");
+
+                    var frequencyLabel2 = document.querySelectorAll(".frequency-label")[1];
+                    var frequency2 = document.querySelectorAll(".frequency")[1];
+                
+                    if (EODP === chosenPlan || DP === chosenPlan) {
+                        walkingForm.style.display = 'flex';
+                        sittingForm.style.display = 'none';
+                        groomingForm.style.display = 'none';
+                        rideForm.style.display = 'none';
+                        frequencyLabel2.style.display = 'none';
+                        frequency2.style.display = 'none';
+                        m15.style.display = '';
+                        m30.style.display = '';
+                        m45.style.display = 'none';
+                        hr.style.display = 'none';
+                    } else {
+                        walkingForm.style.display = 'flex';
+                        sittingForm.style.display = 'none';
+                        groomingForm.style.display = 'none';
+                        rideForm.style.display = 'none';
+                        frequencyLabel2.style.display = '';
+                        frequency2.style.display = '';
+                        m15.style.display = 'none';
+                        m30.style.display = 'none';
+                        m45.style.display = '';
+                        hr.style.display = '';
+                    }
+                })
+            })
+        };
+
+        if (petsGrooming === chosenService) {
+            planHeading.style.visibility = 'visible';
+            groomingPlan.style.display = 'flex';
+            walkingPlan.style.display = 'none';
+            sittingPlan.style.display = 'none';
+            chosenPlan.forEach(chosenPlan => {
+                chosenPlan.addEventListener('click', () => {
+                    var prevChoice = document.querySelector(".action-card");
+                    if (prevChoice) {
+                        prevChoice.classList.remove("action-card");
+                    } chosenPlan.classList.add("action-card");
+                        groomingForm.style.display = 'flex';
+                        sittingForm.style.display = 'none';
+                        walkingForm.style.display = 'none';
+                        rideForm.style.display = 'none';
+                })
+            })
+        };
+
+        if (petsRide === chosenService ) {
+            planHeading.style.visibility = 'hidden';
+            walkingPlan.style.display = 'none';
+            sittingPlan.style.display = 'none';
+            groomingPlan.style.display = 'none';
+            rideForm.style.display = 'flex';
+            sittingForm.style.display = 'none';
+            walkingForm.style.display = 'none';
+            groomingForm.style.display = 'none';
+        } 
+
+    })
+});
