ec46174bc5c8e08feda1105044648a48259b8b75
Stephanie <steph118@protonmail.com>
2023-05-22 13:04:26 +0800

    removed register btn after login

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -4,9 +4,11 @@ if (username) {
     const notLogIn = document.querySelector("#not-login-nav");
     const LogIn =  document.querySelector("#login-nav");
     const showUsername = document.querySelector(".user");
+    const registerBtn = document.querySelector("#register-btn");
     notLogIn.style.display = 'none';
     LogIn.style.display = 'block';
     showUsername.innerHTML = username;
+    registerBtn.style.display = 'none';
 } else {
     const notLogIn = document.querySelector("#not-login-nav");
     const LogIn =  document.querySelector("#login-nav");
